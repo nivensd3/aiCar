a542539964448d31aeae0b7e04f26a25d13d1575
nivensd3 <114529955+nivensd3@users.noreply.github.com>
2024-07-26 15:35:33 -0400

    Create Command.py

---
+++ Command.py
@@ -0,0 +1,16 @@
+class COMMAND:
+    CMD_MOTOR = "CMD_MOTOR"
+    CMD_M_MOTOR = "CMD_M_MOTOR"
+    CMD_CAR_ROTATE = "CMD_CAR_ROTATE"
+    CMD_LED = "CMD_LED"
+    CMD_LED_MOD = "CMD_LED_MOD"
+    CMD_SERVO = "CMD_SERVO"
+    CMD_BUZZER = "CMD_BUZZER"
+    CMD_SONIC = "CMD_SONIC"
+    CMD_LIGHT = "CMD_LIGHT"
+    CMD_POWER = "CMD_POWER" 
+    CMD_MODE ="CMD_MODE"
+    CMD_SENSOR = "CMD_SENSOR"
+    CMD_BOUNDARY = "CMD_BOUNDARY#"
+    def __init__(self):
+        pass
